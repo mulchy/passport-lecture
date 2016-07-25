2c5d9a8b6eedc216dd762a3a15419e51e3d1130a
Ryan Mulcahy <ryan@primeacademy.io>
2016-07-25 13:33:08 -0500

    Basic express app.

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+  console.log('Server listening on ' + server.address().port);
+})
