620b49beb5f15f85941be936737c1819fd11cd05
Ryan Mulcahy <ryan@primeacademy.io>
2016-07-25 13:49:26 -0500

    Missing semicolon.

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   console.log('Server listening on ' + server.address().port);
-})
+});
